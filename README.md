d6a8f1cd39517e44f345b8cd0a676f74f41af9f6
hanzel98 <hanzel.anchia.mena@cr.ey.com>
2021-07-06 23:02:24 -0600

    feat: initial commit with basic tests working

---
+++ README.md
@@ -0,0 +1,15 @@
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`npm install`
+
+## Compile Contracts
+
+`npm compile`
+
+## Run Tests
+
+`npm test`
---
+++ scripts/generate-merkle-root.js
@@ -0,0 +1,19 @@
+const { program } =  require('commander');
+const fs =  require('fs');
+const { parseBalanceMap } =  require('../src/parse-balance-map');
+
+
+program
+  .version('0.0.0')
+  .requiredOption(
+    '-i, --input <path>',
+    'input JSON file location containing a map of account addresses to string balances'
+  )
+
+program.parse(process.argv);
+
+const json = JSON.parse(fs.readFileSync(program.input, { encoding: 'utf8' }));
+
+if (typeof json !== 'object') throw new Error('Invalid JSON');
+
+console.log(JSON.stringify(parseBalanceMap(json)));
